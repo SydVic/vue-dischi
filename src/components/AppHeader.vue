a03ebbf2343131569cfe46769767abe667097d42
SydVic <fabrizio.bocca27@gmail.com>
2022-05-19 14:02:50 +0200

    aggiunto header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my_header d-flex align-items-center ps-4">
+    <img src="../assets/img/logo-small.svg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+.my_header {
+  height: 80px;
+  background-color: #2e3a46;
+
+  img {
+    height: 50px;
+  }
+}
+</style>
