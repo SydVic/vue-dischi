aa0e0d2f4314b9be0a0e173ec83f4dc55a277f06
SydVic <fabrizio.bocca27@gmail.com>
2022-05-20 08:55:39 +0200

    sistemata visualizzazione disc card

--- src/components/DiscCard.vue
+++ src/components/DiscCard.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="my_disc-card text-center mt-4 pt-3">
-    <div class="my_disc-img-wrapper">
-      <img :src="disc.poster" :alt="disc.title + ' ' + disc.author">
+    <div class="my_card-wrapper h-100 px-3 pt-3">
+      <div class="my_disc-img-wrapper">
+        <img :src="disc.poster" :alt="disc.title + ' ' + disc.author">
+      </div>
+      <h4 class="my_disc-title text-uppercase mt-3">{{disc.title}}</h4>
+      <div class="my_disc-author pt-3">{{disc.author}}</div>
+      <div class="my_disc-year pb-2">{{disc.year}}</div>
     </div>
-    <h4 class="my_disc-title text-uppercase mt-3">{{disc.title}}</h4>
-    <div class="my_disc-author pt-3">{{disc.author}}</div>
-    <div class="my_disc-year pb-2">{{disc.year}}</div>
   </div>
 </template>
 
@@ -19,7 +21,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.my_disc-card {
+.my_card-wrapper {
   background-color: #2e3a46;
 
   h4 {
